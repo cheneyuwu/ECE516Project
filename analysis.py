891651d9a6ac7c2d3eb5b02ee3f6095cc988088c
cheneyuwu <cheneyuwu@gmail.com>
2020-04-13 23:55:37 -0400

    results

--- analysis.py
+++ analysis.py
@@ -32,44 +32,51 @@ plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
 
 
 # # Load data from file
+fs = 1e3  # the assumed sample frequency of all data
 data_dir = osp.join(os.getcwd(), "HDR_audio_24GHz_complex_radar")
-data = dict()  # a dictionary containing different gains for HDR measurement
+data_dict = dict()  # a dictionary containing different gains for HDR measurement
 
-# file_name = "roomtone.txt"
-file_name = "dropping_1_object.txt"
-# file_name = "dropping_2_objects.txt"
-# file_name = "dropping_digikey_resistor_bags.txt"
-# file_name = "drop_resistor_tape.txt"
-csvdata = np.empty((8, 0))
-with open(osp.join(data_dir, file_name), newline="") as f:
-    reader = csv.reader(f, delimiter="\t")
-    for row in reader:
-        row = np.array([float(r) for r in row[:8]]).reshape(-1, 1)
-        csvdata = np.append(csvdata, row, axis=1)  # data of shape [8, number of samples]
-data["dropping_1_object_DC10"] = np.stack((csvdata[0, ...], csvdata[0 + 4, ...]))
-data["dropping_1_object_AC100"] = np.stack((csvdata[1, ...], csvdata[1 + 4, ...]))
-data["dropping_1_object_AC1000"] = np.stack((csvdata[2, ...], csvdata[2 + 4, ...]))
-data["dropping_1_object_AC10000"] = np.stack((csvdata[3, ...], csvdata[3 + 4, ...]))
-
-
+for file_name in ["dropping_1_object"]:
+    csvdata = np.empty((8, 0))
+    with open(osp.join(data_dir, file_name + ".txt"), newline="") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            row = np.array([float(r) for r in row[:8]]).reshape(-1, 1)
+            csvdata = np.append(csvdata, row, axis=1)  # data of shape [8, number of samples]
+    data_dict[file_name + "_DC10"] = np.stack((csvdata[0, ...], csvdata[0 + 4, ...]))
+    data_dict[file_name + "_AC100"] = np.stack((csvdata[1, ...], csvdata[1 + 4, ...]))
+    data_dict[file_name + "_AC1000"] = np.stack((csvdata[2, ...], csvdata[2 + 4, ...]))
+    data_dict[file_name + "_AC10000"] = np.stack((csvdata[3, ...], csvdata[3 + 4, ...]))
+    # ###
+    # # normalize data
+    # for k, data in data_dict.items():
+    #     mean = data.mean(axis=1, keepdims=True)
+    #     data = data - mean
+    #     # center the data
+    #     cov = np.cov(data)
+    #     L = np.linalg.cholesky(cov)
+    #     data_dict[k] = np.linalg.inv(L) @ data
+    # ###
+    data_dict[file_name + "_AC10000"] = np.mean([k for k in data_dict.values()], axis=0)
 
 # Generate some arbitrary data
-fs = 1e4
 N = 1e4
 time = np.arange(N) / float(fs)
-sig = transforms.q_chirplet(time, 0.5, 250, 500, 0.1)#  + transforms.q_chirplet(time, 0.5, -250, -500, 0.1)
+sig = transforms.q_chirplet(time, 0.5, 250, 500, 0.1)  #  + transforms.q_chirplet(time, 0.5, -250, -500, 0.1)
 # Select data of format (2, n) real and imaginary
-data["Signal: Chirplet tc=0.5, fc=250, c=500, delta_t=0.1"] = np.stack((sig.real, sig.imag))
+data_dict["Signal: Chirplet tc=0.5, fc=250, c=500, delta_t=0.1"] = np.stack((sig.real, sig.imag))
 
 
 # Working on data
-data_name = "Signal: Chirplet tc=0.5, fc=250, c=500, delta_t=0.1"
-# data_name = "dropping_1_object_AC100"
-data = data[data_name]
+# data_name = "Signal: Chirplet tc=0.5, fc=250, c=500, delta_t=0.1"
+data_name = "dropping_1_object_AC10000"
+data = data_dict[data_name]
+# data = data[:, 4500:5500]
+time = np.arange(data.shape[1]) / float(fs)
 
 
 nrow = 2
-ncoln = 5
+ncoln = 3
 fig, axs = plt.subplots(nrow, ncoln)
 fig.suptitle(data_name, fontsize=BIGGER_SIZE)
 fig.set_size_inches(ncoln * 5, nrow * 5)
@@ -91,70 +98,76 @@ ax.set_xlabel("real")
 ax.set_ylabel("imag")
 ax.set_title("Real vs Imag, Uncalibrated")
 
-f, t, Sxx = signal.spectrogram(data[0, ...] + 1j * data[1, ...], return_onesided=False, fs=fs)
+
+f, t, Sxx = signal.spectrogram(
+    data[0, ...] + 1j * data[1, ...], return_onesided=False, fs=fs, window=("tukey", 0.25), nperseg=128, noverlap=112,
+)
 ax = axs[0][2]
-ax.pcolormesh(t, fftshift(f), np.log(fftshift(Sxx, axes=0)))
+ax.pcolormesh(t, fftshift(f), np.log10(fftshift(Sxx, axes=0)))
 ax.set_ylabel("Frequency [Hz]")
 ax.set_xlabel("Time [sec]")
 ax.set_title("Spectrogram, Uncalibrated")
 
-# generate chirp
-sample = 200
+# # generate chirp
+# sample = 200
+# xy_range = 10000
 
-fb = np.linspace(-1000, 1000, sample)
-fe = np.linspace(-1000, 1000, sample)
-fb, fe = np.meshgrid(fb, fe)
-window = transforms.q_chirplet_fbe(time, 0.5, fb, fe, 0.1)
-res = np.inner(np.conj(window), data[0] + 1j * data[1])
-ax = axs[0][3]
-ax.contour(fb, fe, np.absolute(res))
-ax.set_xlabel("f begin")
-ax.set_ylabel("f end")
-ax.set_title("Freq Begin vs Freq End, Uncalibrated")
+# fb = np.linspace(-xy_range, xy_range, sample)
+# fe = np.linspace(-xy_range, xy_range, sample)
+# fb, fe = np.meshgrid(fb, fe)
+# window = transforms.q_chirplet_fbe(time, 0.5, fb, fe, 0.1)
+# res = np.inner(np.conj(window), data[0] + 1j * data[1])
+# ax = axs[0][3]
+# ax.contour(fb, fe, np.absolute(res))
+# ax.set_xlabel("f begin")
+# ax.set_ylabel("f end")
+# ax.set_title("Freq Begin vs Freq End, Uncalibrated")
 
-fc = np.linspace(-1000, 1000, sample)
-c = np.linspace(-1000, 1000, sample)
-fc, c = np.meshgrid(fc, c)
-window = transforms.q_chirplet(time, 0.5, fc, c, 0.1)
-res = np.inner(np.conj(window), data[0] + 1j * data[1])
-ax = axs[0][4]
-ax.contour(fc, c, np.absolute(res))
-ax.set_xlabel("center freq")
-ax.set_ylabel("slope")
-ax.set_title("Center Freq vs Slope, Uncalibrated")
-
-
-# # Step 1: calibrate data
-# # subtract mean
-# mean = data.mean(axis=1, keepdims=True)
-# data = data - mean
-# # center the data
-# cov = np.cov(data)
-# L = np.linalg.cholesky(cov)
-# data = np.linalg.inv(L) @ data
+# fc = np.linspace(-xy_range, xy_range, sample)
+# c = np.linspace(-xy_range, xy_range, sample)
+# fc, c = np.meshgrid(fc, c)
+# window = transforms.q_chirplet(time, 0.5, fc, c, 0.1)
+# res = np.inner(np.conj(window), data[0] + 1j * data[1])
+# ax = axs[0][4]
+# ax.contour(fc, c, np.absolute(res))
+# ax.set_xlabel("center freq")
+# ax.set_ylabel("slope")
+# ax.set_title("Center Freq vs Slope, Uncalibrated")
 
 
-# # plot uncalibrated data
-# ax = axs[1][0]
-# ax.plot(data[0, ...], label="real", color="r")
-# ax.plot(data[1, ...], label="imag", color="g")
-# ax.set_xlabel("sample index")
-# ax.set_ylabel("sample value")
-# ax.set_title("Radar Data, Calibrated")
-# ax.legend()
+# Step 1: calibrate data
+# subtract mean
+mean = data.mean(axis=1, keepdims=True)
+data = data - mean
+# center the data
+cov = np.cov(data)
+L = np.linalg.cholesky(cov)
+data = np.linalg.inv(L) @ data
 
-# ax = axs[1][1]
-# ax.scatter(data[0, ...], data[1, ...])
-# ax.set_xlabel("real")
-# ax.set_ylabel("imag")
-# ax.set_title("Real vs Imag, Calibrated")
 
-# f, t, Sxx = signal.spectrogram(data[0, ...] + 1j * data[1, ...], return_onesided=False, fs=fs)
-# ax = axs[1][2]
-# ax.pcolormesh(t, fftshift(f), np.log(fftshift(Sxx, axes=0)))
-# ax.set_ylabel("Frequency [Hz]")
-# ax.set_xlabel("Time [sec]")
-# ax.set_title("Spectrogram, Calibrated")
+# plot uncalibrated data
+ax = axs[1][0]
+ax.plot(data[0, ...], label="real", color="r")
+ax.plot(data[1, ...], label="imag", color="g")
+ax.set_xlabel("sample index")
+ax.set_ylabel("sample value")
+ax.set_title("Radar Data, Calibrated")
+ax.legend()
+
+ax = axs[1][1]
+ax.scatter(data[0, ...], data[1, ...])
+ax.set_xlabel("real")
+ax.set_ylabel("imag")
+ax.set_title("Real vs Imag, Calibrated")
+
+f, t, Sxx = signal.spectrogram(
+    data[0, ...] + 1j * data[1, ...], return_onesided=False, fs=fs, window=("tukey", 0.25), nperseg=128, noverlap=112,
+)
+ax = axs[1][2]
+ax.pcolormesh(t, fftshift(f), np.log(fftshift(Sxx, axes=0)))
+ax.set_ylabel("Frequency [Hz]")
+ax.set_xlabel("Time [sec]")
+ax.set_title("Spectrogram, Calibrated")
 
 plt.savefig(data_name + ".jpg")
 plt.show()
